8b2d501e0b1e9f939290877fc3b44d5820944d8e
Nitesh639 <66905080+Nitesh639@users.noreply.github.com>
2021-11-09 22:25:06 -0600

    On RandomForestRegressor Modal

---
+++ randomforestregressor.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+
+#here uploding the data
+data = pd.read_csv('data.csv')
+#columns need more for the house
+features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot','floors', 'waterfront', 'sqft_above']
+house = data[features]
+val = data.price
+
+#splinting our data sets
+train_house ,test_house ,train_val ,test_val =train_test_split(house,val ,test_size=0.2 ,random_state= 10)
+
+#creating our model
+modal = RandomForestRegressor(n_estimators = 300,random_state=0)
+
+#fitting the model
+modal.fit(train_house,train_val)
+prediction = modal.predict(test_house)
+error = mean_absolute_error(test_val,prediction)
+print(error)                                                    #181392.44145439603
+
+
+
+
+# In this regressor it's take more time as compered to DecisionTree beacuse of this model contained maltipule
+# DecisionTree.
