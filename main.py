e1682ac9a106ced5744f97ac0ee493f778412ad0
Nitesh639 <66905080+Nitesh639@users.noreply.github.com>
2021-11-09 19:04:36 -0600

    train_test_split() method use for good data and also check the error in the model.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
+
+#here uploding the data
 data = pd.read_csv('data.csv')
-# print(data.columns)
+#columns need more for the house
 features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot','floors', 'waterfront', 'sqft_above']
 house = data[features]
 val = data.price
 
+#splinting our data sets
+train_house ,test_house ,train_val ,test_val =train_test_split(house,val ,test_size=0.2 ,random_state= 10)
 
 
-model = DecisionTreeRegressor(random_state=1)
-model.fit(house,val)
-result = model.predict(house)
-print(result)
+
+#creating our model
+model = DecisionTreeRegressor(random_state=10)      #after many train I got random_state =10 is good
+
+#fitting the model
+model.fit(train_house,train_val)
+prediction = model.predict(test_house)
+
+#error in the model
+error = mean_absolute_error(prediction,test_val)
+print(error)
